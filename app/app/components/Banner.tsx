fd6412292f76cec611aff0614fe6ac6d54a67a00
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-12-06 02:11:27 +0000

    desc aside name

---
+++ app/app/components/Banner.tsx
@@ -0,0 +1,14 @@
+export default  function Banner() {
+    return (
+        <div>
+            <div className="md:flex items-center flex-row-reverse">
+                
+                <div className="basis-1/2 text-center text-white">
+                    <h2>Digite /admin na URL</h2>
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+}

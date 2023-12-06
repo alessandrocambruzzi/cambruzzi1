f7aae1f7399c949e0acf48313d124302b013bb83
alessandrocambruzzi <146895401+alessandrocambruzzi@users.noreply.github.com>
2023-12-06 02:03:02 +0000

    fix: banco de dados config 2

--- app/components/Aside.tsx
+++
@@ -1,9 +0,0 @@
-export default function Aside() {
-    return (
-        <div className="w-1/6">
-            <a href="/admin/course">Listar Curso</a> <br/>
-            <a href="/admin/course/new">Cadastrar Curso</a>
-
-        </div>
-    )
-}
--- app/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <header className="text-center text-white py-8 border-b border-[#4d4d4d] md:flex itens-center justify-between">
-            <h4>CADASTRO DE COORDENADORES</h4>
-        
-
-        </header>
-    )
-}
